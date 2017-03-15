6f8445a1e24a7c955e3dff4f44d96e390d4b47d6
vikrantnegi <vikrantnegi007@gmail.com>
2017-03-15 19:29:39 +0530

    footer updates

--- footer.php
+++ footer.php
@@ -14,10 +14,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vikrantnegi2' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'vikrantnegi2' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'vikrantnegi2' ), 'vikrantnegi2', '<a href="http://vikrantnegi.com" rel="designer">Vikrant Negi</a>' ); ?>
+		<div class="site-info container">
+			<small class="copyright">
+				By Vikrant Negi. No rights reserved.
+			</small>
+			<small class="site-version">v 2.0</small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function vikrantnegi2_setup() {
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'vikrantnegi2_custom_background_args', array(
-		'default-color' => 'ffffff',
+		'default-color' => '#fafafa',
 		'default-image' => '',
 	) ) );
 }
