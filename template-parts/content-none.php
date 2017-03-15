fbd2ca75505a28e3e6171d106781e0b1b0bc185f
vikrantnegi <vikrantnegi007@gmail.com>
2017-03-15 18:01:41 +0530

    update content none and css for the same

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,29 +9,64 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="<?php if ( is_404() ) { echo 'error-404'; } else { echo 'no-results'; } ?> not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'vikrantnegi2' ); ?></h1>
+		<h1 class="page-title">
+			<?php
+			if ( is_404() ) { esc_html_e( 'Page not available', 'vikrantnegi2' );
+			} else if ( is_search() ) {
+				/* translators: %s = search query */
+				printf( esc_html__( 'Nothing found for &ldquo;%s&rdquo;', 'vikrantnegi2'), '<em>' . get_search_query() . '</em>' );
+			} else {
+				esc_html_e( 'Nothing Found', 'vikrantnegi2' );
+			}
+			?>
+		</h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'vikrantnegi2' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'vikrantnegi2' ); ?></p>
-			<?php
-				get_search_form();
+			<?php get_search_form(); ?>
 
-		else : ?>
+		<?php elseif ( is_404() ) : ?>
+
+			<p><?php esc_html_e( 'You seem to be lost. To find what you are looking for check out the most recent articles below or try a search:', 'vikrantnegi2' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php else : ?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'vikrantnegi2' ); ?></p>
-			<?php
-				get_search_form();
+			<?php get_search_form(); ?>
 
-		endif; ?>
+		<?php endif; ?>
 	</div><!-- .page-content -->
+
+	<?php
+    if ( is_404() || is_search() ) {
+    ?>
+		<h1 class="page-title secondary-title"><?php esc_html_e( 'Most recent posts:', 'vikrantnegi2' ); ?></h1>
+		<?php
+		// Get the 6 latest posts
+		$args = array(
+			'posts_per_page' => 6
+		);
+		$latest_posts_query = new WP_Query( $args );
+		// The Loop
+		if ( $latest_posts_query->have_posts() ) {
+				while ( $latest_posts_query->have_posts() ) {
+					$latest_posts_query->the_post();
+					// Get the standard index page content
+					get_template_part( 'template-parts/content', get_post_format() );
+				}
+		}
+		/* Restore original Post Data */
+		wp_reset_postdata();
+	} // endif
+	?>
 </section><!-- .no-results -->
